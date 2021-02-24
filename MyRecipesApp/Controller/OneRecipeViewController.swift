7a6bc10e9284138315e4c79fa71d71e482bd8934
artazidele <arta.zidele@gmail.com>
2021-02-24 23:08:28 +0200

    edit title

--- MyRecipesApp/Controller/OneRecipeViewController.swift
+++ MyRecipesApp/Controller/OneRecipeViewController.swift
@@ -17,12 +17,50 @@ class OneRecipeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         titleLabel.text = titleString
-
-        // Do any additional setup after loading the view.
         let appDelegate = UIApplication.shared.delegate as! AppDelegate
         context = appDelegate.persistentContainer.viewContext
     }
     
+    @IBAction func editRecipeTitle(_ sender: Any) {
+        editTitle()
+    }
+    private func editTitle() {
+        
+        
+        let alertController = UIAlertController(title: "Edit Title", message: "Edit Title", preferredStyle: .alert)
+        alertController.addTextField { (textField: UITextField) in
+            textField.placeholder = "Enter the new title of recipe!"
+            textField.autocapitalizationType = .sentences
+            textField.autocorrectionType = .no
+        }
+        var recipeToEdit = [Recipe]()
+        let request: NSFetchRequest<Recipe> = Recipe.fetchRequest()
+        request.predicate = NSPredicate(format: "recipeID == %@", argumentArray: [recipeID])
+        do {
+            let result = try context?.fetch(request)
+            recipeToEdit = result!
+        } catch {
+            fatalError(error.localizedDescription)
+        }
+        let addAction = UIAlertAction(title: "Save", style: .cancel) { (action: UIAlertAction) in
+            let textFieldForTitle = alertController.textFields?.first
+            let recipe = recipeToEdit[0]
+            recipe.setValue(textFieldForTitle?.text, forKey: "title")
+            self.saveData()
+            self.titleLabel.text = textFieldForTitle?.text
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive, handler: nil)
+        alertController.addAction(cancelAction)
+        alertController.addAction(addAction)
+        present(alertController, animated: true)
+    }
+    func saveData() {
+        do {
+            try self.context?.save()
+        } catch {
+            fatalError(error.localizedDescription)
+        }
+    }
     // MARK: - Navigation
     @IBAction func seeIngredients(_ sender: Any) {
         let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
